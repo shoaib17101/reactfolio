7fc0da68d661f69fff68a3776d56646a5e101789
shoaib <shoaibkhan171015393@gmail.com>
2025-11-27 10:28:43 +0500

    few changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
                     <p className="text-xl md:text-2xl font-bold">With 3+ years of experience crafting exceptional WordPress solutions</p>
                     <p className="md:w-1/3 ">Specializing in custom theme development, Elementor Pro, WooCommerce, and transforming Figma designs into pixel-perfect WordPress websites.</p>
                     <div className="flex justify-center gap-3 py-5">
-                        <button className="btn bg-white px-8 text-blue-400 rounded-full hover:bg-gray-100">View Projects</button>
+                        <button className="btn bg-white px-8 text-blue-400 rounded-full hover:bg-gray-100"><a href="#projects">View Projects</a></button>
                         <button className="btn bg-[rgba(33,44,54,0.38)] px-8 text-white hover:text-blue-300 border-0 hover:bg-white rounded-full">Download CV</button>
                     </div>
                     <span className="absolute bottom-10  animate-bounce"><a href="#skills"><FaArrowDown size={22}/> </a></span>
